b1a148ee4cc45a494a2578aec3134d173f223a46
Kenneth Velasquez Org <kennethvelasquez@kinal.org.gt>
2026-08-19 23:21:47 -0600

    feat: Se creo el proyecto integrador de java with ant

---
+++ src/org/kennethvelasquez/system/ClasePrincipal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package org.kennethvelasquez.system;
+
+/**
+ *
+ * @author STEPHRYS
+ */
+public class ClasePrincipal {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
